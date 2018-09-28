bd1c9157d6a430997df03814eaf8de12ff8f9174
ID <djakovic_zg@yahoo.com>
2018-09-28 14:10:47 +0200

    added directory link to connection.js

--- test/connection.js
+++ test/connection.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 
+// Open a mongodb.exe
+// cd C:\Program Files\MongoDB\Server\3.6\bin\
+// mongod.exe
+
 // ES6 Promises
 mongoose.Promise = global.Promise;
 
